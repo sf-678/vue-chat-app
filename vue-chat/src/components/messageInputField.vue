9fa5a916e4aa6163487613d366c2c663c6542431
mipepe <souma072166@gmail.com>
2025-08-31 21:57:33 +0900

    create-input-field ・メッセージ入力送信部分を作成 ・メッセージ入力機能用のコンポーネントファイルを追加

---
+++ vue-chat/src/components/messageInputField.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import sendButton from './sendButton.vue';
+import textArea from './textArea.vue';
+</script>
+
+<template>
+  <div class="bg-white rounded-xl p-4 duration-200 hover:shadow-lg hover:shadow-xl ">
+    <div class="flex space-x-3">
+      <!-- 入力欄エリア -->
+      <textArea />
+      <!-- 送信ボタン -->
+      <sendButton />
+    </div>
+  </div>
+</template>
